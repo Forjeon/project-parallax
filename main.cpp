9d2dd8ecacf59b7293d28dffd906b408ae08062f
Forjeon <forjeon22i7@gmail.com>
2025-08-11 10:34:07 -0600

    Finished CMake tutorial step 1; next is first milestone (remember to change project version to 0.1.0 upon completion)

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "config.h"
+
+int main(int argc, char* argv[]) {
+	std::cout << "Hello, world! I'm Project Parallax." << std::endl;
+	std::cout << "I am currently version " << ProjectParallax_VERSION_MAJOR << '.' << ProjectParallax_VERSION_MINOR << '.' << ProjectParallax_VERSION_PATCH << '.' << std::endl;
+}
