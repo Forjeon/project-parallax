ae2242171f412cc6ff031181842f9e2d6f1c8483
Forjeon <forjeon22i7@gmail.com>
2025-08-11 22:01:17 -0600

    Configured CMake to link ncurses and built a highly simple ncurses toy usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+#include <ncurses.h>
 
 #include "config.h"
 
 int main(int argc, char* argv[]) {
 	std::cout << "Hello, world! I'm Project Parallax." << std::endl;
 	std::cout << "I am currently version " << ProjectParallax_VERSION_MAJOR << '.' << ProjectParallax_VERSION_MINOR << '.' << ProjectParallax_VERSION_PATCH << '.' << std::endl;
+
+	// Initialize ncurses
+	initscr();
+	noecho();
+	cbreak();
+
+	// Print initial lines
+	printw("Line 1");
+	printw("Line 2");
+	printw("Line 3");
+	refresh();
+
+	// Test updating lines without reprinting
+	for (int i = 0; i < 100; ++i) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		move(1, 0);
+		clrtoeol();
+		printw("Line 2 %d", i);
+		move(4, 0);
+		refresh();
+	}
+
+	// Clean up and exit
+	endwin();
 }
